81c2a628e53ff0bdc983337a67c44c8e144f813e
jongheon lee <tlqkfwjrekdgl@gmail.com>
2022-12-22 00:00:02 +0900

    [add] first init

---
+++ src/config/api-config.js
@@ -0,0 +1,6 @@
+export const APIKEY = 'fe633759a7fe6b9929127b001f60d5fb';
+export const kakaoAPI = () => (
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=fe633759a7fe6b9929127b001f60d5fb&libraries=services,clusterer,drawing">
+
+    </script>
+);
