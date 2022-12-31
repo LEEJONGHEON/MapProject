44491e18bfa459b6e56ac6d889192d213058adf8
jongheon lee <tlqkfwjrekdgl@gmail.com>
2022-12-31 15:35:16 +0900

    router change

--- src/component/Router.js
+++ src/component/Router.js
@@ -1,13 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "../App";
+import MainPage from "./MainPage";
+import Aed from "./Map/Aed";
+import Hospital from "./Map/Hospital";
+import Emergency from "./Map/Emergency";
+import Rescue from "./Rescue";
+import MapPicture from "./Map/MapPicture";
+import Board from "./Board/Board";
+import Search from "./Search/Search";
+import Aedsearch from "./Search/Aedsearch";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<App.js/>} />
-                <Route path="/test1" element={<GetLocation/>} />
-
+                <Route path="/Mainpage" element={<MainPage/>} />
+                <Route path="/Aed" element={<Aed/>} />
+                <Route path="/Hospital" element={<Hospital />} />
+                <Route path="/Emergency" element={<Emergency />} />
+                <Route path="/Rescue" element={<Rescue />} />
+                <Route path="/MapPicture" element={<MapPicture />} />
+                <Route path="/Board" element={<Board />} />
+                <Route path="/Search" element={<Search />} />
+                <Route path="/Aedsearch" element={<Aedsearch />} />
             </Routes>
         </BrowserRouter>
     );
--- src/store/XmlStore.js
+++
@@ -1,3 +0,0 @@
-class XmlStore {
-
-}
